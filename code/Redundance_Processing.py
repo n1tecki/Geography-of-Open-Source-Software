22b7e6c0f172efcf2c35108180162878eb35178d
Mariusz Nitecki <mariusz.nitecki@outlook.com>
2021-07-05 18:50:34 +0200

    Update Redundance_Processing.py

--- code/Redundance_Processing.py
+++ code/Redundance_Processing.py
@@ -2,7 +2,7 @@ import logging
 
 batch_nr = 0
 
-logging.basicConfig(filename='/home/codeuser/code/LOGS/batch%s_LOG_Redundance_Processing.log' % batch_nr, 
+logging.basicConfig(filename='LOGS/batch%s_LOG_Redundance_Processing.log' % batch_nr, 
                     filemode='a', format='%(asctime)s [%(levelname)s] - %(message)s', 
                     datefmt='%d-%m-%y %H:%M:%S', level=logging.INFO)
 
@@ -10,13 +10,13 @@ try:
 
 
     # Reading in list of users with redundant entries
-    with open('/home/codeuser/code/data/raw_data/batch%s_redundant_usernames.csv' % batch_nr, 'r') as infile:
+    with open('data/raw_data/batch%s_redundant_usernames.csv' % batch_nr, 'r') as infile:
         # Getting unique usernames
         pure = list(dict.fromkeys(infile))
     
         
     # Exporting unique usernames
-    with open('/home/codeuser/code/data/processed_data/batch%s_usernames.csv' % batch_nr, 'w') as outfile:
+    with open('data/processed_data/batch%s_usernames.csv' % batch_nr, 'w') as outfile:
         for i in pure:
             outfile.write(i)
 
